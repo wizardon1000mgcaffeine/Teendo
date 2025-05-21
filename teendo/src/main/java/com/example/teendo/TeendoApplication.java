b672938bcc2d4f0c782ae6f52523f056cbb5b9b2
Vlukacs0 <vlukacs30@gmail.com>
2025-05-21 17:01:04 +0200

    elso

---
+++ teendo/src/main/java/com/example/teendo/TeendoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeendoApplication.class, args);
+	}
+
+}
---
+++ teendo/src/test/java/com/example/teendo/TeendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
